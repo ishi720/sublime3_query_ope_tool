7dfaa89ef8c575cc99193fc13225235861f95f72
ishi720 <ishizuka720@gmail.com>
2019-02-06 20:15:02 +0900

    urlを操作する拡張機能を追加

---
+++ url_ope.py
@@ -0,0 +1,93 @@
+import sublime
+import sublime_plugin
+
+import urllib.parse
+import re
+
+
+def checkSel(sel_range):
+    """選択範囲の確認
+    """
+    if sel_range.empty():
+        sublime.message_dialog("選択範囲が指定されていません")
+        exit()
+
+
+class encodeUrl(sublime_plugin.TextCommand):
+    """urlのエンコードする
+    """
+    def run(self, edit):
+        # 選択範囲の取得
+        sel_area = self.view.sel()
+
+        # 選択範囲の確認
+        checkSel(sel_area[0])
+
+        # 選択範囲の文字列取得
+        sel_string = self.view.substr(sel_area[0])
+
+        # urlエンコード
+        url = urllib.parse.quote(sel_string)
+
+        # 選択範囲と入替え
+        self.view.replace(edit, sel_area[0], url)
+
+
+class decodeUrl(sublime_plugin.TextCommand):
+    """urlをデコードする
+    """
+    def run(self, edit):
+        # 選択範囲の取得
+        sel_area = self.view.sel()
+
+        # 選択範囲の確認
+        checkSel(sel_area[0])
+
+        # 選択範囲の文字列取得
+        sel_string = self.view.substr(sel_area[0])
+
+        # urlデコード
+        url = urllib.parse.unquote(sel_string)
+
+        # 選択範囲と入替え
+        self.view.replace(edit, sel_area[0], url)
+
+
+class getParaLineBreak(sublime_plugin.TextCommand):
+    """URLをgetパラメータで改行する
+    """
+    def run(self, edit):
+        # 選択範囲の取得
+        sel_area = self.view.sel()
+
+        # 選択範囲の確認
+        checkSel(sel_area[0])
+
+        # 選択範囲の文字列取得
+        sel_string = self.view.substr(sel_area[0])
+
+        # 改行を入れる
+        url = re.sub('(\&|\?|#)', "\n\\1", sel_string)
+
+        # 選択範囲と入替え
+        self.view.replace(edit, sel_area[0], url)
+
+
+class removeLineBreak(sublime_plugin.TextCommand):
+    """改行を削除する
+    """
+    def run(self, edit):
+        # 選択範囲の取得
+        sel_area = self.view.sel()
+
+        # 選択範囲の確認
+        checkSel(sel_area[0])
+
+        # 選択範囲を文字列として取得
+        sel_string = self.view.substr(sel_area[0])
+
+        # 改行を削る
+        url = re.sub('\n', '', sel_string)
+
+        # 選択範囲と入替え
+        self.view.replace(edit, sel_area[0], url)
