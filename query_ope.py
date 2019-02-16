ed16e3c346c3ef237e4fd27299c3e4108b9283a8
ishi720 <ishizuka720@gmail.com>
2019-02-16 21:53:25 +0900

    sqlを改行するコマンドを作成。まずはSELECT文

--- query_ope.py
+++ query_ope.py
@@ -76,6 +76,35 @@ class getParaLineBreak(sublime_plugin.TextCommand):
             self.view.replace(edit, sel_area[i], url)
 
 
+
+class sqlLineBreak(sublime_plugin.TextCommand):
+    """SQLを改行する
+    """
+    def run(self, edit):
+        # 選択範囲の取得
+        sel_area = self.view.sel()
+
+        # 選択範囲の確認
+        checkSel(sel_area[0])
+
+        for i in range(len(sel_area)):
+            # 選択範囲の文字列取得
+            sel_string = self.view.substr(sel_area[i])
+
+            keyword_list = [
+                'SELECT','FROM','WHERE','AND','OR','ORDER BY','LIMIT'
+            ]
+            regexp = '(' + "|".join(keyword_list) + ')'
+
+            # 改行を入れる
+            url = re.sub(regexp, "\n\\1", sel_string, flags=re.IGNORECASE)
+
+            # 戦闘の改行を削る
+            url = re.sub('^\n', '', url)
+
+            # 選択範囲と入替え
+            self.view.replace(edit, sel_area[i], url)
+
 class removeLineBreak(sublime_plugin.TextCommand):
     """改行を削除する
     """
