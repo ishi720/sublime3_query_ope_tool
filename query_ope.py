6ed5f142c5bf0d9159f8a8a20aeeb467c37fbe7d
ishi720 <ishizuka720@gmail.com>
2019-02-18 13:25:28 +0900

    sqlから改行を削除するときにスペースを入れる。SELECT以外にもよく使うsql文も対応する

--- query_ope.py
+++ query_ope.py
@@ -76,35 +76,6 @@ class getParaLineBreak(sublime_plugin.TextCommand):
             self.view.replace(edit, sel_area[i], url)
 
 
-
-class sqlLineBreak(sublime_plugin.TextCommand):
-    """SQLを改行する
-    """
-    def run(self, edit):
-        # 選択範囲の取得
-        sel_area = self.view.sel()
-
-        # 選択範囲の確認
-        checkSel(sel_area[0])
-
-        for i in range(len(sel_area)):
-            # 選択範囲の文字列取得
-            sel_string = self.view.substr(sel_area[i])
-
-            keyword_list = [
-                'SELECT','FROM','WHERE','AND','OR','ORDER BY','LIMIT'
-            ]
-            regexp = '(' + "|".join(keyword_list) + ')'
-
-            # 改行を入れる
-            url = re.sub(regexp, "\n\\1", sel_string, flags=re.IGNORECASE)
-
-            # 戦闘の改行を削る
-            url = re.sub('^\n', '', url)
-
-            # 選択範囲と入替え
-            self.view.replace(edit, sel_area[i], url)
-
 class removeLineBreak(sublime_plugin.TextCommand):
     """改行を削除する
     """
@@ -121,6 +92,58 @@ class removeLineBreak(sublime_plugin.TextCommand):
 
             # 改行を削る
             url = re.sub('\n', '', sel_string)
+
+            # 選択範囲と入替え
+            self.view.replace(edit, sel_area[i], url)
+
+
+class sqlLineBreak(sublime_plugin.TextCommand):
+    """SQLを改行する
+    """
+    def run(self, edit):
+        # 選択範囲の取得
+        sel_area = self.view.sel()
+
+        # 選択範囲の確認
+        checkSel(sel_area[0])
+
+        for i in range(len(sel_area)):
+            # 選択範囲の文字列取得
+            sel_string = self.view.substr(sel_area[i])
+
+            keyword_list = [
+                'SELECT','UPDATE','SET','INSERT INTO','VALUES','DELETE',
+                'FROM','WHERE','AND','OR','ORDER BY','GROUP BY','LIMIT','OFFSET'
+            ]
+            regexp = '(' + "|".join(keyword_list) + ')'
+
+            # 改行を入れる
+            url = re.sub(regexp, "\n\\1", sel_string, flags=re.IGNORECASE)
+
+            # 戦闘の改行を削る
+            url = re.sub('^\n', '', url)
+
+            # 選択範囲と入替え
+            self.view.replace(edit, sel_area[i], url)
+
+
+class sqlRemoveLineBreak(sublime_plugin.TextCommand):
+    """改行を削除する
+    """
+    def run(self, edit):
+        # 選択範囲の取得
+        sel_area = self.view.sel()
+
+        # 選択範囲の確認
+        checkSel(sel_area[0])
+
+        for i in range(len(sel_area)):
+            # 選択範囲を文字列として取得
+            sel_string = self.view.substr(sel_area[i])
+
+            # 整形する
+            url = re.sub('\n', " ", sel_string) # 改行を削る
+            url = re.sub('\s+', " ", url) # スぺースを整える
 
             # 選択範囲と入替え
             self.view.replace(edit, sel_area[i], url)
