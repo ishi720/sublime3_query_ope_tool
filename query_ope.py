17410f7e2a8dd1d09950243a8e2659ed1a885141
ishi720 <ishizuka720@gmail.com>
2020-03-19 11:05:00 +0900

    エクセル表をコピペしてmarkdownテーブルに変換する機能を追加

--- query_ope.py
+++ query_ope.py
@@ -257,7 +257,55 @@ class convertTableMdToExcel(sublime_plugin.TextCommand):
             url = re.sub('\t$', "", url, flags=re.MULTILINE) # 末尾のタブを削る
             url = re.sub('(\S*)<br>(\S*)', lambda m: '"'+ re.sub('<br>','\n', m.group(0),flags=re.MULTILINE) +'"', url, flags=re.MULTILINE) # セル内の改行コードの変換
 
-
-
             # 選択範囲と入替え
             self.view.replace(edit, sel_area[i], url)
+
+
+class convertTableExcelToMd(sublime_plugin.TextCommand):
+    """Excelテーブルをmarkdownテーブルに変換
+    """
+    def run(self, edit):
+        # 選択範囲の取得
+        sel_area = self.view.sel()
+
+        # 選択範囲の確認
+        checkSel(sel_area[0])
+
+        for i in range(len(sel_area)):
+
+            # 選択範囲を文字列として取得
+            sel_string = self.view.substr(sel_area[i])
+
+            # 整形する
+            text = re.sub("\"(.*?)\"", lambda m: re.sub('\n','<br>',m.group(1), flags=re.MULTILINE), sel_string, flags=(re.MULTILINE | re.DOTALL))
+            text = re.sub("^(.+)","| \\1", text, flags=re.MULTILINE )
+            text = re.sub("(.)$","\\1 |", text, flags=re.MULTILINE )
+            text = re.sub("\t"," | ", text, flags=re.MULTILINE )
+
+            # 改行区切りでlistに変換
+            textList = text.splitlines()
+
+            # 選択範囲に空行が含まれることを考慮して、1行目を断定する
+            firstRow = 0
+            for j in range(len(textList)):
+                if textList[j]:
+                    firstRow = j
+                    break
+
+                # 空行しかない場合は処理を終了
+                if j == len(textList)-1:
+                    exit()
+
+            firstLine = textList[firstRow] # 1行目を取得する
+            columNum = firstLine.count('|')-1 # columnの数を計算する
+
+            # 2行目を組み立てる
+            secondLine = "|"
+            for j in range(columNum):
+                secondLine = secondLine + " - |"
+
+            textList.insert(firstRow+1,secondLine) # 2行目を挿入する
+            text = '\n'.join(textList) # listをテキストに戻す
+
+            # 選択範囲と入替え
+            self.view.replace(edit, sel_area[i], text)
